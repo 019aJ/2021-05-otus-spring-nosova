d34bea60fa41a853bb1ff7c978432da8d4046950
019a <019a@mail.ru>
2021-05-30 20:10:09 +0300

    Введение в Spring Framework. Домашнее задание Приложение должно корректно запускаться с помощью "java -jar"

--- student-test-app/src/test/java/QuestionServiceTest.java
+++ student-test-app/src/test/java/QuestionServiceTest.java
@@ -19,16 +19,16 @@ public class QuestionServiceTest {
 
     @Test
     public void parseQuestions() {
-        QuestionServiceCSVImpl service = new QuestionServiceCSVImpl("questions.csv", new QuestionParserServiceCSVImpl());
+        QuestionServiceCSVImpl service = new QuestionServiceCSVImpl("/questions.csv", new QuestionParserServiceCSVImpl());
         List<Question> questions = service.questions();
         assertTrue(questions.size() == 5);
         Question question = questions.get(0);
         assertNotNull(question);
-        assertEquals("Kerry … swim very well yet.", question.getQuestionText());
+        assertEquals("Kerry ... swim very well yet.", question.getQuestionText());
         List<Answer> answers = question.getAnswerVariants();
         assertNotNull(answers);
         assertTrue(answers.size() == 3);
-        assertArrayEquals(new String[]{"shouldn't", "ought", "cannot"}, answers.stream().map(a -> a.getText()).collect(Collectors.toList()).toArray(new String[3]));
+        assertArrayEquals(new String[]{"shouldn't", "ought", "cannot"}, answers.stream().map(Answer::getText).collect(Collectors.toList()).toArray(new String[3]));
     }
 
     @Test
